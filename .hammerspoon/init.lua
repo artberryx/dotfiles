0785cb6ff3d317da4c439b63c16fd3ac6721cddf
Seohong Park <artberryx@naver.com>
2020-08-10 16:11:16 +0900

    hammerspoon에 한영 전환 단축 기능 추가

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,4 +1,6 @@
 local inputEnglish = "com.apple.keylayout.ABC"
+local inputKorean = "com.apple.inputmethod.Korean.2SetKorean"
+local inputJapanese = "com.apple.inputmethod.Kotoeri.Japanese"
 
 function escEnglish()
 	local inputSource = hs.keycodes.currentSourceID()
@@ -8,4 +10,14 @@ function escEnglish()
 	hs.eventtap.keyStroke({}, 'escape')
 end
 
+function toggle()
+	local inputSource = hs.keycodes.currentSourceID()
+	if inputSource == inputEnglish then
+		hs.keycodes.currentSourceID(inputKorean)
+	else
+		hs.keycodes.currentSourceID(inputEnglish)
+	end
+end
+
 hs.hotkey.bind({'control'}, 33, escEnglish)
+hs.hotkey.bind({}, 'f13', toggle)
